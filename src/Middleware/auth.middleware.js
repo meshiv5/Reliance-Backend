f796f2b4a3b6af7daed04aa8e3cb845c621b21be
meshiv5 <000done9@gmail.com>
2022-11-11 06:52:50 +0530

    sdsd

--- src/Middleware/auth.middleware.js
+++ src/Middleware/auth.middleware.js
@@ -1,13 +1,15 @@
 const jwt = require("jsonwebtoken");
-const auth = (req, res, next) => {
+const { User } = require("../Models/user.model");
+const auth = async (req, res, next) => {
   const token = req.headers["x-access-token"] || req.headers["authorization"];
   if (!token) return res.status(401).send("Access denied. No token provided.");
   try {
     const decoded = jwt.verify(token, process.env.privateKey);
-    if (!decoded._id) {
+    const user = await User.findOne({ _id: decoded._id });
+    if (!user) {
       throw "myException";
     }
-    req.user = decoded;
+    req.user = user;
     next();
   } catch (ex) {
     res.status(400).send("Invalid token.");
